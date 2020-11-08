4ce05bab045a0160ee63c5d1f63f77d7251a2cc1
sid <sharda_sudhanshoo@yahoo.co.in>
2020-11-08 01:51:31 -0500

    Leet Code Solution fro problem 17. Backtracking

---
+++ src/main/java/com/algo/backtracking/SumOfSubSets.java
@@ -0,0 +1,70 @@
+package main.java.com.algo.backtracking;
+
+import com.sun.jdi.Value;
+import main.java.com.algo.bst.ValidateBST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * For a given set of an array we want to get
+ * all the subsets of array for which if we consider the value
+ * should equal to a sum.
+ *
+ * Example: {5,10,12,13,15,18}
+ *
+ * Sum total needed: 30
+ *
+ * Ops {1,1,0,0,1,0} 5 + 10 + 15 = 30
+ *     {0,0,1,1,1,0} 12 + 13 + 15 = 30 ...
+ *
+ *  To solve this problem, we will use backtracking.
+ */
+
+
+public class SumOfSubSets {
+
+    private static List<int[]> output = new ArrayList<>();
+    private static final int VALUE = 40;
+
+
+    public static void main(String[] args) {
+
+        int [] numbers = new int [] {5, 10, 12, 13, 15, 18};
+
+        getAllSubSets(numbers, 0, 73, new int [6], 0);
+
+        System.out.println(output.size());
+
+    }
+
+
+    public static void getAllSubSets(int [] numbers, int sum, int balance, int [] solution, int index){
+
+        if(index > numbers.length -1)
+            return;
+
+        int number = numbers[index];
+
+        if(checkSumAndBalance(number, sum, balance)) {
+            solution[index] = 1;
+            if(sum + number == VALUE){
+                output.add(solution.clone());
+            }
+            getAllSubSets(numbers, sum + number, balance - number, solution, index + 1);
+
+        }
+
+        solution[index] = 0;
+        getAllSubSets(numbers, sum, balance - number, solution, index + 1);
+
+    }
+
+    public static boolean checkSumAndBalance(int number, int sum, int balance){
+
+        if(number + sum <= VALUE && sum + (balance - number) >= 0)
+            return true;
+        return false;
+
+    }
+}
